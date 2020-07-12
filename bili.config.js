fa9fffc7f0c8a9f9686d06300e3b14245df61a00
Mehran Majidi <mehran.majidi@cafebazaar.ir>
2020-07-12 19:52:44 +0430

    chore: update buid configs

--- bili.config.js
+++ bili.config.js
@@ -1,5 +1,8 @@
 module.exports = {
   output: {
+    fileName({ format }) {
+      return `async-actions[min]${format !== 'cjs' ? '.[format]' : ''}[ext]`;
+    },
     format: ['esm', 'cjs'],
   },
 };
