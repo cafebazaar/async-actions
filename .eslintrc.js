c0fef6ee7b82a58c82e5e4ecca5cbcce250e136b
Mehran Majidi <mehran.majidi@cafebazaar.ir>
2020-07-12 18:24:30 +0430

    chore: add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2020: true,
+  },
+  extends: ['standard', 'prettier'],
+  parserOptions: {
+    ecmaVersion: 11,
+    sourceType: 'module',
+  },
+  rules: {},
+};
