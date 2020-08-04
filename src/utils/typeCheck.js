522555b2c606da161319709a5229b1ef02e0a725
Mehran Majidi <mehran.majidi@cafebazaar.ir>
2020-08-04 22:41:20 +0430

    feat(utils): add isFunction function

--- src/utils/typeCheck.js
+++ src/utils/typeCheck.js
@@ -1,6 +1,9 @@
-/* eslint-disable */
 export function isNumber(num) {
   return (
     (typeof num === 'number' && !Number.isNaN(num)) || num instanceof Number
   );
+}
+
+export function isFunction(val) {
+  return val && {}.toString.call(val) === '[object Function]';
 }
